4fa309ef2d8c946e0c7cc8275c0ec4e3a9415a5a
digpatel123 <digpatel123@gmail.com>
2023-01-19 14:54:51 +0000

    Creating different patterns with turtle.

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+import random
 from turtle import Turtle, Screen
 
 jimmy = Turtle()
 jimmy.shape("turtle")
 jimmy.color("blueviolet")
-for i in range(10):
-    jimmy.pendown()
-    jimmy.forward(10)
-    jimmy.penup()
-    jimmy.forward(10)
+colors = ["brown", "khaki", "wheat", "yellow", "orange", "tan", "red2", "dark green", "indigo", "firebrick"]
+
+d = 360
+for i in range(3,11):
+    angle = d/i
+    jimmy.color(random.choice(colors))
+    while i != 0:
+        jimmy.forward(100)
+        jimmy.right(angle)
+        i -= 1
+
+
 
 screen = Screen()
 
