64000895a086d571517921fc05c63c3ff067d509
digpatel123 <digpatel123@gmail.com>
2023-01-23 13:14:14 +0000

    Added method to create a spirograph

--- main.py
+++ main.py
@@ -6,19 +6,26 @@ jimmy = Turtle()
 jimmy.shape("turtle")
 turtle.colormode(255)
 
+
 def random_color():
-    r = random.randint(0,255)
+    r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
     return (r, g, b)
 
+
 jimmy.speed("fastest")
 angle = 0
-for i in range(72):
-    jimmy.setheading(angle)
-    jimmy.color(random_color())
-    jimmy.circle(100)
-    angle += 5
+
+
+def spirograph(angle_gap):
+    for i in range(int(360 / angle_gap)):
+        jimmy.color(random_color())
+        jimmy.circle(100)
+        jimmy.setheading(jimmy.heading() + angle_gap)
+
+
+spirograph(5)
 
 screen = turtle.Screen()
 screen.exitonclick()
