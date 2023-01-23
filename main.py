9513bfde53b01070acea0b0472ab0c2a47740ecd
digpatel123 <digpatel123@gmail.com>
2023-01-23 12:36:13 +0000

    Created method to generate random color.

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import random
+import turtle
 from turtle import Turtle, Screen
 
 jimmy = Turtle()
 jimmy.shape("turtle")
-jimmy.color("blueviolet")
-colors = ["brown", "khaki", "wheat", "yellow", "orange", "tan", "red2", "dark green", "indigo", "firebrick"]
+turtle.colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+
 directions = [0, 90, 180, 270]
 jimmy.pensize(10)
 jimmy.speed(20)
 for i in range(200):
-    jimmy.color(random.choice(colors))
+    jimmy.color(random_color())
     jimmy.setheading(random.choice(directions))
     jimmy.forward(20)
 
